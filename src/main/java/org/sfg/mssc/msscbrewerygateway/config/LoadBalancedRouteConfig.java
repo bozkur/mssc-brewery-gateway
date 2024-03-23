499147cfb8329fb704d657407606d79f85661fea
cevher.bozkur <bozkur@gmail.com>
2024-03-23 18:13:52 +0300

    Added Resilience4J circuit breaker for inventory.
    Closes #6

--- src/main/java/org/sfg/mssc/msscbrewerygateway/config/LoadBalancedRouteConfig.java
+++ src/main/java/org/sfg/mssc/msscbrewerygateway/config/LoadBalancedRouteConfig.java
@@ -20,7 +20,13 @@ public class LoadBalancedRouteConfig {
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("lb://beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(f -> f.circuitBreaker(c ->
+                                c.setName("inventoryCB")
+                                        .setFallbackUri("forward:/inventory-failover")
+                                        .setRouteId("inv-failover")))
                         .uri("lb://beer-inventory-service"))
+                .route(r -> r.path("/inventory-failover")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
 }
